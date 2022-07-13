cd1e876b057c55fc207048927e7aab0f1d5c41b1
yenngann <109246127+yenngann@users.noreply.github.com>
2022-07-14 03:14:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'Em';
+const nameGirl = 'Anh yêu';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng Duy đẹp trai';
+const contentCard = 'Chúc anh yêu sinh nhật vui vẻ, mau chóng mua nhà để mình có thể sống thoải mái hơn. Yêu em nhiều hơn. hehe';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
